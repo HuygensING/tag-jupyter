72a537db1661b881e9b0c3c8fa209be1fff4236c
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2020-09-29 11:30:14 +0200

    update version to 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val artifactId: String = "tag-jupyter"
 
 group = "nl.knaw.huygens.tag"
-version = "0.1.2"
+version = "0.1.3"
 
 plugins {
     kotlin("jvm") version "1.4.0"
